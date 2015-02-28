469120aaf6537aab640353dfc4de2c1436d94bb6
Gustavo Jose Morales Carpio <gustavo.morales@gmail.com>
2015-02-28 18:31:20 +0000

    Fix and Create tests form Contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
     belongs_to :user
+    
+    validates :name, presence: true
+    validates :lastname, presence: true
 end
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "que el contacto no se guarde vacio" do
+    contact = Contact.new
+    assert !contact.save, "el contacto se guardo vacio"
+  end
+  
+  test "que el contacto se guarde con nombre y apellido" do
+    contact = Contact.new
+    contact.name =  'Pedro'
+    assert_not contact.save, "el contacto se guarda con nombre pero sin apellido"
+  end
+  
+  test "que el contacto se guarde con nombre y/o apellido" do
+    contact = Contact.new
+    contact.lastname =  contacts(:one).lastname
+    assert_not contact.save, "el contacto se guarda con nombre pero sin apellido"
+  end
 end
