ddbee5a82ac7a699f2c215ae2fd8d0376a03f044
Gustavo Morales <gustavo.morales@gmail.com>
2015-02-28 12:42:11 +0000

    Fix test and delete user's controller and views

--- app/views/users/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @user, :id, :name, :lastname, :created_at, :updated_at
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test "the post requires name and lastname" do
+  #   contact = Contact.new
+  #   assert !contact.save, "The contact was saved without required fields"
+
+  #   puts "--Errors--"
+  #   contact.errors.full_messages.each { |msg| puts msg }
   # end
 end
